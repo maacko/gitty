08c93ff3005891086cee5bac5dc254431f5566b0
Marco Siguencia <m08251983@gmail.com>
2017-05-03 12:01:30 -0400

    The Home Component
    The design, if you can even call it that, of the Home component is tentative. I
    didn't want to leave it empty so I put up something simple. I'll revisit the
    design once I get the barebones of the app working.
    
    /index.css:
            - Adding the styling for the Home Component
    
    /components/Home.js:
            - A simple component consisting of a button and some text

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Home (props) {
     return (
-        <div>
-            Home
+        <div id='home-container'>
+            <h1>Gitty</h1>
+            <h2>a code ditty about github</h2>
+            <Link className='button' to='/battle'>Let's Go!</Link>
         </div>
     )
 }
