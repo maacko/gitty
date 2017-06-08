62d42917ef2e8f16640978df00175b89e7b9b8da
Marco Siguencia <m08251983@gmail.com>
2017-06-08 00:23:05 -0400

    Implementing the Battle Routine and it's Subroutines
    We left off with creating a preview component that allows a user to preview
    their submission. Users may change their mind after making a submission,
    therefore, we allow them to reset their selection. Once the users are happy with
    their submissions, they can confirm by pressing "Battle". This will cause the
    router to route to a results component, representing a new phase in the battle
    process. Since we have the results component render as a separate component, and
    not a child of battle,  it doesn't have access to battle's state. Therefore, we
    pass the appropriate parameters, through the uri, as query string. React-router
    allows us to access a match object, an object created describing the properties
    (url, query, etc.) of a matched route, which helps us to greatly in composing a
    uri for the results component.
    
    The goal is to create a results component that is able to render the statistics
    of the two submitted users, along with their profile name and avatar. In
    addition, we also want to calculate a winner and a score based on these
    stats. In order to accomplish this, the component will require the means to
    fetch the data and calculate a score to determine the winner. This commit
    contains a collection of functions designed to do exactly that.
    
    The functions added to the api_helpers module are Promise-based functions; they
    allow us to fetch and manipulate data into a usable form for our components
    without creating 'callback hell'. We, again, make use of axios to create async
    http get requests to fetch data from the GitHub API.
    
    In order to create this battle function that promises, on completion, to pass
    over an array of user objects, complete with their score and statistics, we need
    to compose it out of smaller functions.
    
    Note: Be sure to read this excellent article on Promises:
    https://developers.google.com/web/fundamentals/getting-started/primers/promises
    
    Most of these smaller functions are kept private, in the sense that they are
    never exported, and are composed into a larger function called getUserInfo.
    getUserInfo, with the help of these smaller functions, fetches the user's
    profile and repos from GitHub and crafts a new object containing the user's
    profile and the score computed from the repos.
    
    With the expectation that getUserInfo will fetch the user's info, battle invokes
    the function for each of the user names it receives and promises, on completion,
    to pass over a sorted array -- in descending order, so that we can pick out the
    winner by referencing the first element in the array. Since battle immediately
    returns with a Promise object -- recall that Promises are async structures and,
    therefore, return immediately after invocation/creation --, when we invoke
    battle, we'll need to tack on a callback for when the promise completes.
    
    As you can guess, we're going to export and use battle in the Results component
    to fetch the data we need. We'll pass battle the submitted user names extracted
    from the uri's query string, as an array.
    
    app/utils/api_helpers.js:
            - Defining a bunch of helper functions that fetch data from GitHub using
              the GitHub API
                    - These helper functions are Promise-based, and because battle is a
                      composition of these Promise-based functions, battle must also
                      return a promise.
    
            - Adding helper functions that sum up the number of stars each user in
              their repos.

--- app/utils/api_helpers.js
+++ app/utils/api_helpers.js
@@ -1,5 +1,74 @@
 import axios from 'axios'
 
+const getProfile = function (username) {
+
+    var url = 'https://api.github.com/users/' + username;
+
+    return axios.get(url).then(function (response) {
+        //the data is an object, the object is filled with properties describing
+        //the user
+        return response.data;
+    });
+};
+
+const getRepos = function (username) {
+
+    var url = 'https://api.github.com/users/' + username + '/repos';
+
+    return axios.get(url).then(function (response) {
+        //the data is an array of repos, each repo is an object filled with
+        //properties describing the repo
+        return response.data;
+    });
+};
+
+const starCount = function (repos) {
+    return repos.reduce(function (count, repo) {
+       return count + repo.stargazers_count;
+    }, 0);
+};
+
+const getUserInfo = function (username) {
+
+    return axios.all([
+        getProfile(username),
+        getRepos(username)
+    ]).then(function (data) {
+
+        var profile = data[0];
+        var totalStars = starCount(data[1]);
+        var score = calculateScore(profile.followers, totalStars);
+
+        return {
+            profile: profile,
+            score: score,
+        }
+    });
+};
+
+const calculateScore = function (followers, stars) {
+    return (3*followers)*stars;
+}
+
+const sortUsers = function (users) {
+
+    return users.sort(function compare(a, b) {
+        //b - a because we want to sort in decreasing order. Use a - b if we want
+        //to sort in increasing order.
+        return (b.score - a.score);
+    });
+}
+
+const handleError = function (error) {
+
+    console.warn(error);
+    return null;
+}
+
+/* Note to self: The use of const isn't there to imply that APIHelpers is
+ * immutable. Rather, it just a good habit to use const and let when using
+ * ES2015+ syntax.
+ */
 const APIHelpers = {
     /* getPopularRepos takes in a selected language and returns a promise to
      * pass the popular repos to the callback we append to the callback chain.
@@ -22,6 +91,19 @@ const APIHelpers = {
              */
             return response.data.items;
         });
+    },
+    /* battle takes in a list of user names and returns a Promise that promises,
+     * if the get procedures are successful, to pass a sorted list of user
+     * objects -- each containing the user's profile information and a calculated
+     * score based on the number of followers and stars -- to the next callback
+     * in the chain. Since battle returns a Promise, we can add another callback
+     * to the chain to make use of the results. In the case that one of the
+     * get operations fail, the error is propagated down the chain, caught
+     * by the catch, and handled by handleError.
+     */
+    battle (usernames) {
+        return axios.all(usernames.map(getUserInfo)).then(sortUsers)
+            .catch(handleError);
     }
 }
 
